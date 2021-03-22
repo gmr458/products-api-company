cc78be74dc39e0ae85faf83a079c2e03ea291124
German Marin Rolong <germanmarinrolong@gmail.com>
2021-03-21 22:31:01 -0500

    modelo de producto finalizado

---
+++ src/models/Product.js
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    brand: String,
+    model: String,
+    category: String,
+    price: Number,
+    imgURL: String,
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default model("Product", productSchema);
