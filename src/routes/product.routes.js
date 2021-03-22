97f80a6205c50e56fe20405095b7f46ec5fcda31
German Marin Rolong <germanmarinrolong@gmail.com>
2021-03-21 22:32:07 -0500

    rutas de los productos finalizadas

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,13 +3,19 @@ import * as productControllers from "../controllers/product.controllers";
 
 const router = Router();
 
-router.route("/")
-  .get(productControllers.getProducts)
+router.route("/get-all")
+  .get(productControllers.getProducts);
+
+router.route("/create")
   .post(productControllers.createProduct);
 
-router.route("/:productId")
-  .get(productControllers.getProductByiD)
-  .put(productControllers.updateProductByiD)
+router.route("/get/:productId")
+  .get(productControllers.getProductByiD);
+
+router.route("/update/:productId")
+  .put(productControllers.updateProductByiD);
+
+router.route("/delete/:productId")
   .delete(productControllers.deleteProductByiD);
 
 export default router;
